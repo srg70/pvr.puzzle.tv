748140ff48b814cefd0b4802fef7b22d34d4a30b
Sergey Shramchenko <sergey.shramchenko@gmail.com>
2017-04-25 14:38:54 +0300

    Support android-arm platform.

---
+++ cmake/FindCURL.cmake
@@ -0,0 +1,33 @@
+#.rst:
+# FindCurl
+# --------
+# Finds the Curl library
+#
+# This will will define the following variables::
+#
+# CURL_FOUND - system has Curl
+# CURL_INCLUDE_DIRS - the Curl include directory
+# CURL_LIBRARIES - the Curl libraries
+# CURL_DEFINITIONS - the Curl definitions
+#
+# and the following imported targets::
+#
+#   Curl::Curl   - The Curl library
+
+if(PKG_CONFIG_FOUND)
+  pkg_check_modules(PC_CURL libcurl QUIET)
+endif()
+
+find_path(CURL_INCLUDE_DIR NAMES curl/curl.h NO_CMAKE_FIND_ROOT_PATH
+                           PATHS ${PC_CURL_INCLUDEDIR})
+find_library(CURL_LIBRARY NAMES curl libcurl NO_CMAKE_FIND_ROOT_PATH
+                          PATHS ${PC_CURL_LIBDIR})
+
+set(CURL_VERSION ${PC_CURL_VERSION})
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Curl
+                                  REQUIRED_VARS CURL_LIBRARY CURL_INCLUDE_DIR
+                                  VERSION_VAR CURL_VERSION)
+
+mark_as_advanced(CURL_INCLUDE_DIR CURL_LIBRARY)
