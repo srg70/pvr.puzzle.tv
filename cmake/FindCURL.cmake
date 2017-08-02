b962af6043b81ffbf43f7253ef7d00dddf0d2405
Sergey Shramchenko <sergey.shramchenko@gmail.com>
2017-08-02 19:22:00 +0300

    Prebuilt CURL library for ios. Temporary: just for TVOS only. TODO: add for IOS.

--- cmake/FindCURL.cmake
+++ cmake/FindCURL.cmake
@@ -8,6 +8,7 @@
 # CURL_FOUND - system has Curl
 # CURL_INCLUDE_DIRS - the Curl include directory
 # CURL_LIBRARIES - the Curl libraries
+# CURL_SSL_LIBRARIES - the Curl SSL libraries
 # CURL_DEFINITIONS - the Curl definitions
 #
 # and the following imported targets::
@@ -21,12 +22,19 @@ endif()
 if(CORE_SYSTEM_NAME STREQUAL osx)
     find_path(CURL_INCLUDE_DIRS NAMES curl/curl.h
                                PATHS ${PC_CURL_INCLUDEDIR})
-    find_library(CURL_LIBRARIES NAMES curl libcurl 
-                              PATHS ${PC_CURL_LIBDIR})
+    find_library(CURL_LIBRARIES NAMES curl libcurl
+                                PATHS ${PC_CURL_LIBDIR})
+    find_library(CURL_SSL_LIBRARIES NAMES ssl libssl
+                                PATHS ${PC_CURL_LIBDIR})
 else()
-    find_path(CURL_INCLUDE_DIRS NAMES curl/curl.h NO_CMAKE_FIND_ROOT_PATH
+    find_path(CURL_INCLUDE_DIRS NAMES curl/curl.h
+                    NO_CMAKE_FIND_ROOT_PATH
                     PATHS ${PC_CURL_INCLUDEDIR})
-    find_library(CURL_LIBRARIES NAMES curl libcurl libcurl_a NO_CMAKE_FIND_ROOT_PATH
+    find_library(CURL_LIBRARIES NAMES curl libcurl libcurl_a
+                    NO_CMAKE_FIND_ROOT_PATH
+                    PATHS ${PC_CURL_LIBDIR})
+    find_library(CURL_SSL_LIBRARIES NAMES ssl libssl libssl_a
+                    NO_CMAKE_FIND_ROOT_PATH
                     PATHS ${PC_CURL_LIBDIR})
 
 endif()
@@ -39,4 +47,4 @@ find_package_handle_standard_args(Curl
                                   REQUIRED_VARS CURL_LIBRARIES CURL_INCLUDE_DIRS
                                   VERSION_VAR CURL_VERSION)
 
-mark_as_advanced(CURL_INCLUDE_DIRS CURL_LIBRARIES)
+mark_as_advanced(CURL_INCLUDE_DIRS CURL_LIBRARIES CURL_SSL_LIBRARIES)
