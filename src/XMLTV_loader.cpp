157201b1716840a1256984e68ade3e9d6c9afc05
Sergey Shramchenko <sergey.shramchenko@gmail.com>
2020-09-14 19:36:02 +0300

    fix: timezone offset detection on Windows.

--- src/XMLTV_loader.cpp
+++ src/XMLTV_loader.cpp
@@ -256,7 +256,10 @@ namespace XMLTV {
         _get_timezone(&offset);
         int daylightHours = 0;
         _get_daylight(&daylightHours);
-        offset += daylightHours;
+        XBMC->Log(LOG_DEBUG, "Timezone offset: %d sec, daylight offset %d h", offset, daylightHours);
+        offset += daylightHours * 60 * 60;
+        XBMC->Log(LOG_DEBUG, "Total timezone offset: %d sec", offset);
+
 #endif // TARGET_WINDOWS
 
         return offset;
